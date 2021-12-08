dd97a467cae92a526dda21aea341b7d78fcb428d
renatorussobr <renrusso@gmail.com>
2021-12-08 09:36:35 -0500

    commit

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,29 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+class iPad {
+    constructor(
+        //define parameters
+        size,
+        generation,
+        owner,
+        color
+    ){
+    //define properties
+    this.size = size;
+    this.generation = generation;
+    this.owner = owner;
+    this.color = {
+        inside: insideColor,
+        outside: outsideColor, 
+    };
+    this.turnOn = turnOn;
+    }
+//Add methods like normal functions:
+    switchOn(powerStatus) {
+        this.turnOn = powerStatus;
+    }
+}
+
+export default iPad
